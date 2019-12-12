7ec06c3f0bc9694181879647d2975c7a0976fedd
algebraic-designer-6218 <jmiller@archersystems.us>
2019-12-12 12:51:20 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+require 'pry'
+def line(deli)
+  if array(deli) == []
+    puts "The line is currently empty."
+
+  if ! array == []
+    puts "The line is currently: 1. Logan, 2. Avi 3. Spenser"
+  end
+  
